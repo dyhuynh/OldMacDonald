c3bef55f4dd0e906c2c84395d0bdd4626d84bb89
dyhuynh <dyhuynh@s.sfusd.edu>
2018-10-18 07:59:54 -0700

    done

--- Farm.java
+++ Farm.java
@@ -1,12 +1,12 @@
 class Farm 
 {
-private Animal[] allMyAnimals = new Animal[3];
+private Animal[] allMyAnimals = new Animal[4];
 
 public Farm() {
-	allMyAnimals[0] = new Cow("spaghetti","Moo");
+	allMyAnimals[0] = new Cow("Spaghetti","Moo");
 	allMyAnimals[1] = new Chick("Chick","cheep","chirp");
 	allMyAnimals[2] = new NamedCow("Cow","MOO?","Bobby");
-
+	allMyAnimals[3] = new Pig("Bacon", "OINKK!!");
 		}	 
 
 public void animalSounds() {
--- Pig.java
+++ Pig.java
@@ -1,4 +1,12 @@
-class Pig //implements Animal 
-{     
-     //your code here
+class Pig implements Animal
+{     private String myType;
+	  private String mySound;
+
+	Pig(String pType,String pSound) {
+		myType = pType;
+		mySound = pSound;
+	}
+
+	public String getType() {return myType;}
+	public String getSound() {return mySound;}
 }
